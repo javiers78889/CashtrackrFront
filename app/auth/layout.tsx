efa701f7b58507196b76649975167e7e6a2417ce
javiers78889 <javiers78889@gmail.com>
2025-02-13 13:28:16 -0500

    fin.

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,5 +1,6 @@
 import Logo from "@/components/ui/Logo";
 import ToasterNotification from "@/components/ui/ToasterNotification";
+import Link from "next/link";
 
 
 export default function AuthLayout({
@@ -13,7 +14,10 @@ export default function AuthLayout({
             <div className="lg:grid lg:grid-cols-2 lg:min-h-screen ">
                 <div className="bg-purple-950 flex justify-center lg:bg-auth lg:bg-30 bg-no-repeat bg-left-bottom">
                     <div className="py-10 lg:py-20 ">
-                        <Logo />
+                        <Link href={'/'}>
+
+                            <Logo />
+                        </Link>
                     </div>
                 </div>
 
